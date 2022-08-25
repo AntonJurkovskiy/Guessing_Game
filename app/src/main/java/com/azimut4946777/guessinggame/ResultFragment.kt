b55f945f420acd28cc2200f1b46d95e74a1e2a8c
AntonJurkovskiy <azimut4946777@gmail.com>
2022-08-25 12:42:01 +0300

    added viewModelFactory

--- app/src/main/java/com/azimut4946777/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/azimut4946777/guessinggame/ResultFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.azimut4946777.guessinggame.databinding.FragmentGameBinding
 import com.azimut4946777.guessinggame.databinding.FragmentResultBinding
@@ -13,6 +14,9 @@ import com.azimut4946777.guessinggame.databinding.FragmentResultBinding
 class ResultFragment : Fragment() {
  private var _binding: FragmentResultBinding?=null
     private val binding get() = _binding!!
+lateinit var viewModel: ResultViewModel
+lateinit var viewModelFactory: ResultViewModelFactory
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +24,13 @@ class ResultFragment : Fragment() {
     ): View? {
        _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this,viewModelFactory).get(ResultViewModel::class.java)
+        binding.wonLost.text = viewModel.result
+
+
+
         binding.newGameButton.setOnClickListener {
             view.findNavController()
                 .navigate(R.id.action_resultFragment_to_gameFragment)
